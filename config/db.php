34a23d17ea39c4807ad9b845f180999eb502734e
jingtianW1ng <ajssjaus@gmail.com>
2025-02-08 05:46:27 +1030

    set up sql serve

--- config/db.php
+++ config/db.php
@@ -1,9 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'task_manager';
-$username = 'root';
-$password = '';
+$host = 'sql306.infinityfree.com';
+$db   = 'if0_38266485_task_manager';
+$user = 'if0_38266485';
+$pass = 'cIafwyJzSmjG';
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
