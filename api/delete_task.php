6b3a1f8600448749b0ef9802674fd0edf890676c
jingtianW1ng <ajssjaus@gmail.com>
2025-02-09 23:00:45 +1030

    Fix full dir

--- api/delete_task.php
+++ api/delete_task.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../config/db.php';
-require_once __DIR__ . '/security/security.php';
+require_once __DIR__ . '/../security/security.php';
 session_start();
 header('Content-Type: application/json');
 
