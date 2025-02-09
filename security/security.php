465157ccb26f259a4a54b432314f6eea5e9b4df5
jingtianW1ng <ajssjaus@gmail.com>
2025-02-09 23:01:06 +1030

    Fix session already start

--- security/security.php
+++ security/security.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 /** force https avoid MITM */
 //if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
